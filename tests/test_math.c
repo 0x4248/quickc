2ee05bf977a172edc3ce65e51952d8ba9139045a
awesomelewis2007 <awesomelewis2007@gmail.com>
2023-03-12 12:39:47 +0000

    Add math test and fix  hello world test

---
+++ tests/test_math.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    printf("1+1 = %f", 1+1);
+    printf("sin(30) = %f", sin(30));
+    printf("sqrt(2*2 + 4*4) = %f", sqrt(2*2 + 4*4));
+    printf("1+2*3 = %f", 1+2*3);
+    return 0;
+}
