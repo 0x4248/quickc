eb18fb38028b36bb3d95e69dc64cefe95ae12310
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-05-24 21:45:58 +0100

    Update comments
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- src/include/help.h
+++ src/include/help.h
@@ -1,6 +1,8 @@
 /* Quick C (help.h)
  * A quick C and C++ runner
  * Github: https://www.github.com/awesomelewis2007/quickc
+ * License: GNU General Public License v3.0
+ * By: Lewis Evans
  *
  * Help function
  */
--- src/include/logger.h
+++ src/include/logger.h
@@ -1,6 +1,8 @@
 /* Quick C (logger.c)
  * A quick C and C++ runner
  * Github: https://www.github.com/awesomelewis2007/quickc
+ * License: GNU General Public License v3.0
+ * By: Lewis Evans
  *
  * Logging functions
  */
