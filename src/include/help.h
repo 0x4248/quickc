267fc031f5c7c4a4abb826fe445a3f93e0aef3b4
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-05-10 20:55:28 +0100

    Update help.h
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- src/include/help.h
+++ src/include/help.h
@@ -17,9 +17,9 @@
 void help() {
     std::cout << "Usage: quickc <File> <Flags file> <args>" << std::endl;
     std::cout << "Options:" << std::endl;
-    std::cout << "  -quickv  Verbose" << std::endl;
-    std::cout << "  -quickl  Log to file" << std::endl;
-    std::cout << "  -quickh  Show this help" << std::endl;
+    std::cout << "  -quickv  Verbose output" << std::endl;
+    std::cout << "  -quickl  Log output to file" << std::endl;
+    std::cout << "  -quickh  Shows this help message" << std::endl;
     std::cout << "Other flags:" << std::endl;
-    std::cout << "  Any other flags will be sent to gcc or g++" << std::endl;
+    std::cout << "  Any other flags will be passed to gcc or g++" << std::endl;
 }
